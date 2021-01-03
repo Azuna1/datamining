84d96bfee1fa3d39228d10a7142f4a9ab3eb2443
Timo <timoziv@googlemail.com>
2021-01-03 13:23:38 +0100

    init

---
+++ cluster-tree.R
@@ -0,0 +1,52 @@
+library(arules)
+library(rpart)
+library(rpart.plot)
+
+# to remove all objects 
+rm(list = ls()) 
+
+#pfad für unseren projektordner
+path = "C://Users//azunai//Desktop//Uni//DataMining//Projekt"
+setwd(path)
+
+coffeeTable = read.delim("kaffee.asc",sep=" ")
+
+#datentyp der einzelnen spalten korrigieren
+coffeeTable[[1]] <- as.factor(coffeeTable[[1]])
+coffeeTable[[2]] <- as.factor(coffeeTable[[2]])
+coffeeTable[[4]] <- as.factor(coffeeTable[[4]])
+coffeeTable[[6]] <- as.factor(coffeeTable[[6]])
+coffeeTable[[7]] <- as.factor(coffeeTable[[7]])
+coffeeTable[[8]] <- as.factor(coffeeTable[[8]])
+coffeeTable[[10]] <- as.factor(coffeeTable[[10]])
+coffeeTable[[11]] <- as.factor(coffeeTable[[11]])
+coffeeTable[[12]] <- as.factor(coffeeTable[[12]])
+
+# numerische Daten standardisieren
+nummer.stand = (coffeeTable$nummer - mean(coffeeTable$nummer)) / sd(coffeeTable$nummer)
+dauer.stand = (coffeeTable$dauer - mean(coffeeTable$dauer)) / sd(coffeeTable$dauer)
+persn.stand = (coffeeTable$persn - mean(coffeeTable$persn)) / sd(coffeeTable$persn)
+
+coffeeTable.stand = data.frame(coffeeTable$treue,coffeeTable$menge,coffeeTable$preis, nummer.stand,coffeeTable$marke,
+                               dauer.stand,coffeeTable$alter,coffeeTable$klasse,coffeeTable$einkm,persn.stand,coffeeTable$prbew,
+                               coffeeTable$bildg)
+
+# entscheidungsbaum (irgendwas stimmt hier gewaltig nicht....)
+tree.stand = rpart(coffeeTable.treue ~ .,
+             method="class",
+             dat=coffeeTable.stand,
+             minsplit=2,
+             minbucket=1
+             
+)
+rpart.plot(tree.stand, uniform=TRUE, main="Markentreu?", cex=.6)
+
+#vergleich ohne standardisierung
+tree = rpart(treue ~ .,
+             method="class",
+             dat=coffeeTable,
+             minsplit=2,
+             minbucket=1
+)
+rpart.plot(tree, uniform=TRUE, main="Markentreu?", cex=.6)
+#rpart.plot(tree, type=3, extra=0, branch.lty=3, box.palette="RdYlGn", main="markentreu?", nn=TRUE,cex=.6)
---
+++ warenkorb.R
@@ -0,0 +1,62 @@
+library(arules)
+
+
+# to remove all objects 
+rm(list = ls()) 
+
+#pfad für unseren projektordner
+path = "C://Users//azunai//Desktop//Uni//DataMining//Projekt"
+setwd(path)
+
+coffeeTable = read.delim("kaffee.asc",sep=" ")
+
+## datentyp der einzelnen spalten korrigieren
+coffeeTable[[1]] <- as.factor(coffeeTable[[1]])
+coffeeTable[[2]] <- as.factor(coffeeTable[[2]])
+coffeeTable[[4]] <- as.factor(coffeeTable[[4]])
+coffeeTable[[6]] <- as.factor(coffeeTable[[6]])
+coffeeTable[[7]] <- as.factor(coffeeTable[[7]])
+coffeeTable[[8]] <- as.factor(coffeeTable[[8]])
+coffeeTable[[10]] <- as.factor(coffeeTable[[10]])
+coffeeTable[[11]] <- as.factor(coffeeTable[[11]])
+coffeeTable[[12]] <- as.factor(coffeeTable[[12]])
+
+#rows bennenen für übersicht
+row.names(coffeeTable) = paste("T",c(1:130986),"",sep="")
+
+coffeeTrans = as(coffeeTable,"transactions")
+summary(coffeeTrans)
+itemFrequency(coffeeTrans)
+itemFrequencyPlot(coffeeTrans)
+
+#### daten auswerten
+## verteilung
+itemFrequency(coffeeTrans)
+itemFrequencyPlot(coffeeTrans)
+
+
+## Definition:
+# The support is the probability that all items in the rule will be
+# found together in a checkout basket in relation to the total
+# number of transactions.
+
+# The confidence of a rule is the probability of finding socks in a
+# checkout basket that contains shoes.
+
+
+rules = apriori(coffeeTrans, parameter = list(supp=0.15, conf=0.4, target="rules", minlen=2))
+inspect(rules)
+inspect(sort(rules, by="lift")[1:10])
+inspect(sort(rules, by="support")[1:10])
+inspect(sort(rules, by="confidence")[1:10])
+
+## top treu
+rulesTreu = subset(rules, rhs %in% "treue=1")
+inspect(rulesTreu)
+inspect(sort(rulesTreu, by="confidence")[1:10])
+
+
+## Top untreu
+rulesUntreu = subset(rules, rhs %in% "treue=2")
+inspect(rulesUntreu)
+inspect(sort(rulesUntreu, by="confidence")[1:10])
