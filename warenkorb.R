b003f875df0f9503d484965c39ba35fbc95d80f2
Timo <timoziv@googlemail.com>
2021-01-05 12:19:00 +0100

    warenkorb.R auswertung

--- warenkorb.R
+++ warenkorb.R
@@ -1,4 +1,7 @@
 library(arules)
+library(arulesViz)
+library(tibble)
+library(ggplot2)
 
 
 # to remove all objects 
@@ -25,6 +28,8 @@ row.names(coffeeTable) = paste("T",c(1:130986),"",sep="")
 
 coffeeTrans = as(coffeeTable,"transactions")
 summary(coffeeTrans)
+inspect(head(coffeeTrans))
+
 itemFrequency(coffeeTrans)
 itemFrequencyPlot(coffeeTrans)
 
@@ -32,7 +37,7 @@ itemFrequencyPlot(coffeeTrans)
 
 #### daten auswerten
 
-## verteilung 
+## generelle verteilung 
 itemFrequency(coffeeTrans)
 itemFrequencyPlot(coffeeTrans)
 
@@ -41,34 +46,185 @@ itemFrequencyPlot(coffeeTrans)
 # The support is the probability that all items in the rule will be
 # found together in a checkout basket in relation to the total
 # number of transactions.
+# -> relative Häufigkeit der Beispiele, in denen die Regel anwendbar ist
 
 # The confidence of a rule is the probability of finding socks in a
 # checkout basket that contains shoes.
+# ->  relative Häufigkeit der Beispiele, in denen die Regel richtig ist
+
+# Der Lift gibt an, wie hoch der Konfidenzwert für die Regel den Erwartungswert übertrifft,
+# er zeigt also die generelle Bedeutung einer Regel
+
+#
 
 
-rules = apriori(coffeeTrans, parameter = list(supp=0.15, conf=0.4, target="rules", minlen=2))
-inspect(rules)
-inspect(sort(rules, by="lift")[1:10])
-inspect(sort(rules, by="support")[1:10])
-inspect(sort(rules, by="confidence")[1:10])
+rules.2 = apriori(coffeeTrans, parameter = list(supp=0.1, conf=0.1, target="rules", minlen=2))
+summary(rules.2)
 
-## top treu
-rulesTreu = subset(rules, rhs %in% "treue=1")
-inspect(rulesTreu)
-inspect(sort(rulesTreu, by="confidence")[1:10])
+# THE TOP-N RULES
+inspect(head(sort(rules.2, by="lift", decreasing=TRUE),10))
+
+plot(rules.2)
+
+## zum vergleich mit minlen = 3
+rules.3 = apriori(coffeeTrans, parameter = list(supp=0.1, conf=0.1, target="rules", minlen=3))
+summary(rules.3)
+
+# THE TOP-N RULES
+inspect(head(sort(rules.3, by="lift", decreasing=TRUE),10))
+
+plot(rules.3)
+
+## zum vergleich mit minlen = 4
+rules.4 = apriori(coffeeTrans, parameter = list(supp=0.1, conf=0.1, target="rules", minlen=4))
+summary(rules.4)
+
+# THE TOP-N RULES
+inspect(head(sort(rules.4, by="lift", decreasing=TRUE),10))
+
+plot(rules.4)
+
+# minlen = 4 scheint zu komplex, wir nutzen minlen = 3
+
+## visualisierung wie sich der support verhält bezogen auf confidence = 0.1
+minSupport = seq(0.05, 0.9, 0.05)
+total = c()
+for(support in minSupport){
+  rules.test = apriori(coffeeTrans, parameter=list(support=support, confidence=0.1, minlen=3, target="rules"))
+  total = c(total,length(rules.test))
+}
+
+rule2support = tibble(minSupport,total)
+rule2support
+
+ggplot(rule2support,aes(x=minSupport,y=total)) + geom_line() + geom_point() + labs(title="confidence=0.1", x="minimum support",y="# of rules") + theme_light()
+
+## visualisierung wie sich der support verhält bezogen auf confidence = 0.2
+minSupport = seq(0.05, 0.9, 0.05)
+total = c()
+for(support in minSupport){
+  rules.test = apriori(coffeeTrans, parameter=list(support=support, confidence=0.2, minlen=3, target="rules"))
+  total = c(total,length(rules.test))
+}
+
+rule2support = tibble(minSupport,total)
+rule2support
+
+ggplot(rule2support,aes(x=minSupport,y=total)) + geom_line() + geom_point() + labs(title="confidence=0.2", x="minimum support",y="# of rules") + theme_light()
+
+## visualisierung wie sich der support verhält bezogen auf confidence = 0.3
+minSupport = seq(0.05, 0.9, 0.05)
+total = c()
+for(support in minSupport){
+  rules.test = apriori(coffeeTrans, parameter=list(support=support, confidence=0.3, minlen=3, target="rules"))
+  total = c(total,length(rules.test))
+}
+
+rule2support = tibble(minSupport,total)
+rule2support
+
+ggplot(rule2support,aes(x=minSupport,y=total)) + geom_line() + geom_point() + labs(title="confidence=0.3", x="minimum support",y="# of rules") + theme_light()
 
 
-## Top untreu
-rulesUntreu = subset(rules, rhs %in% "treue=2")
-inspect(rulesUntreu)
-inspect(sort(rulesUntreu, by="confidence")[1:10])
+## visualisierung wie sich der support verhält bezogen auf confidence = 0.4
+minSupport = seq(0.05, 0.9, 0.05)
+total = c()
+for(support in minSupport){
+  rules.test = apriori(coffeeTrans, parameter=list(support=support, confidence=0.4, minlen=3, target="rules"))
+  total = c(total,length(rules.test))
+}
+
+rule2support = tibble(minSupport,total)
+rule2support
+
+ggplot(rule2support,aes(x=minSupport,y=total)) + geom_line() + geom_point() + labs(title="confidence=0.4", x="minimum support",y="# of rules") + theme_light()
+
+## visualisierung wie sich der support verhält bezogen auf confidence = 0.5
+minSupport = seq(0.05, 0.9, 0.05)
+total = c()
+for(support in minSupport){
+  rules.test = apriori(coffeeTrans, parameter=list(support=support, confidence=0.5, minlen=3, target="rules"))
+  total = c(total,length(rules.test))
+}
+
+rule2support = tibble(minSupport,total)
+rule2support
+
+ggplot(rule2support,aes(x=minSupport,y=total)) + geom_line() + geom_point() + labs(title="confidence=0.5", x="minimum support",y="# of rules") + theme_light()
+
+####
+# Der support verhält sich jeweils gleich, bis 0.3 confidence werden ca gleich viele regeln gefunden
+# über 0.4 reduziert sich die anzahl stark, daher wählen wir für confidence 0.4 und 0.5 im vergleich
+# Für support scheint der Bereich zwischen 0.1 und 0.2 interessant, da hier ebenfalls die Anzahl der Regeln stark abnimmt
+
+### support 0.1
+rules.3 = apriori(coffeeTrans, parameter = list(supp=0.1, conf=0.4, target="rules", minlen=3))
+summary(rules.3)
+
+# THE TOP-N RULES
+inspect(head(sort(rules.3, by="lift", decreasing=TRUE),10))
+
+plot(rules.3)
+
+# top treu by lift
+rulesTreu = subset(rules.3, rhs %in% "treue=1")
+inspect(head(rulesTreu))
+inspect(sort(rulesTreu, by="lift")[1:10])
+
+
+# Top untreu by lift
+rulesUntreu = subset(rules.3, rhs %in% "treue=2")
+inspect(head(rulesUntreu))
+inspect(sort(rulesUntreu, by="lift")[1:10])
+
+
+### support = 0.15
+
+rules.3 = apriori(coffeeTrans, parameter = list(supp=0.15, conf=0.4, target="rules", minlen=3))
+summary(rules.3)
+
+# THE TOP-N RULES
+inspect(head(sort(rules.3, by="lift", decreasing=TRUE),10))
+
+plot(rules.3)
+
+# top treu by lift
+rulesTreu = subset(rules.3, rhs %in% "treue=1")
+inspect(head(rulesTreu))
+inspect(sort(rulesTreu, by="lift")[1:10])
+
+
+# Top untreu by lift
+rulesUntreu = subset(rules.3, rhs %in% "treue=2")
+inspect(head(rulesUntreu))
+inspect(sort(rulesUntreu, by="lift")[1:10])
+
+### support = 0.2
+
+rules.3 = apriori(coffeeTrans, parameter = list(supp=0.2, conf=0.4, target="rules", minlen=3))
+summary(rules.3)
+
+# THE TOP-N RULES
+inspect(head(sort(rules.3, by="lift", decreasing=TRUE),10))
+
+plot(rules.3)
+
+# top treu by lift
+rulesTreu = subset(rules.3, rhs %in% "treue=1")
+inspect(head(rulesTreu))
+inspect(sort(rulesTreu, by="lift")[1:10])
+
+
+# Top untreu by lift
+rulesUntreu = subset(rules.3, rhs %in% "treue=2")
+inspect(head(rulesUntreu))
+inspect(sort(rulesUntreu, by="lift")[1:10])
 
 
 
 
 
-
-## falls fehler "Fehler in length(x) : Method length not implemented for class rules"
+#### falls fehler "Fehler in length(x) : Method length not implemented for class rules"
 # in konsole folgende befehle ausführen
 # unloadNamespace("arules")
 # update.packages("arules")
